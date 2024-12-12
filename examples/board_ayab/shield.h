360b2f9ee2d3b99f16d045ba36f9d000a0ebc7ce
Jonathan Perret <j-github@jonathanperret.net>
2024-12-12 07:32:38 +0100

    Add serial monitoring

--- examples/board_ayab/shield.h
+++ examples/board_ayab/shield.h
@@ -15,9 +15,19 @@
 #define PIN_HALL_LEFT  ADC_IRQ_ADC1
 
 typedef struct {
+	int len;
+	char prefix;
+	uint8_t buf[128];
+} slipmsg_t;
+
+typedef struct {
     int led[2];
     char beeper_history[16];
     i2c_mcp23008_t mcp23008[2];
+
+	slipmsg_t slip_msg_in;
+	slipmsg_t slip_msg_out;
+	char slip_history[110];
 } shield_t;
 
 #endif
