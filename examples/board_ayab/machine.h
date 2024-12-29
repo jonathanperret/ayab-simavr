2853fd92cd2eac46f96c32ba05193bf5ff1cf128
Jonathan Perret <j-github@jonathanperret.net>
2024-12-29 21:51:37 +0100

    Introduce test mode

--- examples/board_ayab/machine.h
+++ examples/board_ayab/machine.h
@@ -8,8 +8,10 @@
 #define BELT_PHASE_ADVANCE 8
 
 #define MARGIN_NEEDLES 28
+#define MAX_NEEDLES 200
 
 enum side {LEFT, RIGHT};
+enum direction {LEFTWARDS, RIGHTWARDS};
 enum machine_type {KH910, KH930, KH270};
 enum carriage_type {KNIT, LACE, GARTER, KNIT270};
 enum belt_phase_type {REGULAR, SHIFTED};
@@ -17,6 +19,8 @@ enum belt_phase_type {REGULAR, SHIFTED};
 typedef struct {
     enum carriage_type type;
     int position;
+    enum direction direction;
+    int selected_needle;
 } carriage_t;
 
 typedef struct {
@@ -25,12 +29,18 @@ typedef struct {
     int sensor_radius;
     int num_needles;
     int num_solenoids;
-    carriage_t carriage;
     int belt_phase;
+
+    carriage_t carriage;
+    unsigned encoder_phase;
+    int belt_phase_signal;
     int hall_left, hall_right;
+    char needles[MAX_NEEDLES];
     uint16_t solenoid_states;
+    uint16_t previous_solenoid_states;
     uint16_t armature_states;
     uint16_t previous_armature_states;
+    int dirty;
 } machine_t;
 
 #endif
